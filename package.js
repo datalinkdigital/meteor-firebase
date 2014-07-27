ca09b7c5a664c51a82dd06be3614fc9f504bd95e
datalinkdigital <hendra.charles@gmail.com>
2014-07-27 12:06:11 +0800

    Update package.js

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   summary: "Firebase wrapper for Meteor"
 });
 
-Npm.depends({ "firebase": ">=0.6.15" });
+Npm.depends({ "firebase": "1.0.18" });
 
 Package.on_use(function(api) {
   if (api.export) api.export('Firebase', 'server');
